5965c5e3c6367e23eb6f8251151723526e1b1763
Steven G. Harms <steven.harms@flatironschool.com>
2019-08-20 10:42:10 -0400

    Remove clashing migration names

--- db/migrate/005_create_characters.rb
+++
@@ -1,9 +0,0 @@
-class CreateCharacters < ActiveRecord::Migration[5.1]
-  def change
-    create_table :characters do |t|
-      t.string :name
-      t.string :show_id
-      t.string :actor_id
-    end
-  end
-end
